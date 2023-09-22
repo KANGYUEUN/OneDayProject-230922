464bb1d473812f1fea1b85976330a4980739fbde
KANGYUEUN <yu1128eun@kakao.com>
2023-09-22 12:29:21 +0900

    0922

---
+++ lib/OrderManagementScreen.dart
@@ -0,0 +1,125 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:oneday/main.dart';
+
+class OrderManagementScreen extends StatefulWidget {
+  final OrderTable table;
+
+  const OrderManagementScreen({super.key, required this.table});
+
+  @override
+  State<OrderManagementScreen> createState() => _OrderManagementScreenState();
+}
+
+class _OrderManagementScreenState extends State<OrderManagementScreen> {
+  final TextEditingController _orderController = TextEditingController();
+  final TextEditingController _editOrderController =
+      TextEditingController(); // 추가: 수정을 위한 컨트롤러
+  bool isEditing = false; // 추가: 항목 수정 모드를 나타내는 변수
+  int editingIndex = -1; // 추가: 수정 중인 항목의 인덱스
+
+  @override
+  void dispose() {
+    _orderController.dispose();
+    super.dispose();
+  }
+
+  void _addToOrderList() {
+    final String item = _orderController.text;
+    if (item.isNotEmpty) {
+      setState(() {
+        widget.table.orderList.add(item);
+        _orderController.clear();
+      });
+    }
+  }
+
+  void _removeFromOrderList(String item) {
+    setState(() {
+      widget.table.orderList.remove(item);
+    });
+  }
+
+  void _startEditing(int index) {
+    setState(() {
+      isEditing = true;
+      editingIndex = index;
+      _editOrderController.text = widget.table.orderList[index];
+    });
+  }
+
+  void _saveEdit() {
+    final String editedItem = _editOrderController.text;
+    if (editedItem.isNotEmpty) {
+      setState(() {
+        widget.table.orderList[editingIndex] = editedItem;
+        isEditing = false;
+        editingIndex = -1;
+        _editOrderController.clear();
+      });
+    }
+  }
+
+  void _cancelEdit() {
+    setState(() {
+      isEditing = false;
+      editingIndex = -1;
+      _editOrderController.clear();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(' ${widget.table.tableNumber} 테이블 주문 관리'),
+      ),
+      body: Column(
+        children: <Widget>[
+          TextField(
+            controller: isEditing ? _editOrderController : _orderController,
+            decoration: const InputDecoration(labelText: '주문 항목'),
+          ),
+          ElevatedButton(
+            onPressed: isEditing ? _saveEdit : _addToOrderList,
+            child: Text(isEditing ? '저장' : '주문 추가'),
+          ),
+          if (isEditing)
+            ElevatedButton(
+              onPressed: _cancelEdit,
+              child: const Text('취소'),
+            ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.table.orderList.length,
+              itemBuilder: (context, index) {
+                final item = widget.table.orderList[index];
+                return ListTile(
+                  title: Text(item),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      IconButton(
+                        icon: const Icon(Icons.edit),
+                        onPressed: () {
+                          _startEditing(index);
+                        },
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          _removeFromOrderList(item);
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:oneday/OrderManagementScreen.dart';
 
 void main() {
   runApp(const App());
@@ -20,27 +21,95 @@ class App extends StatelessWidget {
   }
 }
 
-class TableManagement extends StatelessWidget {
+class TableManagement extends StatefulWidget {
   const TableManagement({super.key});
+
+  @override
+  State<TableManagement> createState() => _TableManagementState();
+}
+
+class _TableManagementState extends State<TableManagement> {
+  List<bool> isTableSelected = List.generate(9, (index) => false);
+  List<OrderTable> orderTables = List.generate(
+      9, (index) => OrderTable(tableNumber: index + 1, orderList: []));
+
+  void toggleTableSelection(int index) {
+    setState(() {
+      isTableSelected[index] = !isTableSelected[index];
+      if (isTableSelected[index]) {
+        _openOrderManagementScreen(orderTables[index]);
+      }
+    });
+  }
+
+  void _openOrderManagementScreen(OrderTable table) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => OrderManagementScreen(table: table),
+      ),
+    );
+  }
+
+  // void addToOrderList(String item, double price) {
+  //   setState(() {
+  //     orderList.add(item);
+  //     totalAmount += price;
+  //   });
+  // }
+
+  // void removeFromOrderList(String item, double price) {
+  //   setState(() {
+  //     orderList.remove(item);
+  //     totalAmount -= price;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3, // 3x3 그리드
+        crossAxisCount: 2,
       ),
       itemBuilder: (BuildContext context, int index) {
-        // 각 셀에 원하는 내용을 배치할 수 있습니다.
-        return Card(
-          margin: const EdgeInsets.all(8.0),
-          child: Center(
-            child: Text(
-              '테이블 ${index + 1}',
-              style: const TextStyle(fontSize: 18.0),
+        final isSelected = isTableSelected[index];
+        final tableNumber = index + 1;
+
+        return GestureDetector(
+          onTap: () {
+            toggleTableSelection(index);
+          },
+          child: Card(
+            margin: const EdgeInsets.all(18.0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            color: isSelected ? Colors.green : Colors.white,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  '테이블 $tableNumber',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: isSelected ? Colors.white : Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                isSelected
+                    ? const Icon(Icons.check_box, color: Colors.red)
+                    : const SizedBox.shrink(),
+              ],
             ),
           ),
         );
       },
     );
   }
+}
+
+class OrderTable {
+  final int tableNumber;
+  final List<String> orderList;
+
+  OrderTable({required this.tableNumber, required this.orderList});
 }
